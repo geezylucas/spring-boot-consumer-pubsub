91c5c6da0d820add313600b774d9c09d4f0ddc29
Gerardo Lucas Cruz <sorec@live.com>
2024-09-24 00:43:43 -0600

    feat: add UserMessage

--- src/main/java/com/geezylucas/springbootconsumerpubsub/PubSubController.java
+++ src/main/java/com/geezylucas/springbootconsumerpubsub/PubSubController.java
@@ -1,6 +1,10 @@
 package com.geezylucas.springbootconsumerpubsub;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.geezylucas.springbootconsumerpubsub.model.Body;
+import com.geezylucas.springbootconsumerpubsub.model.UserMessage;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
@@ -13,11 +17,16 @@ import java.util.Base64;
 // PubsubController consumes a Pub/Sub message.
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PubSubController {
+
+    private final ObjectMapper objectMapper;
 
     @PostMapping("/")
     public Mono<ResponseEntity<String>> receiveMessage(@RequestBody Body body) {
         // Get PubSub message from request body.
+        log.info("Message received: {}", body);
+
         Body.Message message = body.getMessage();
         if (message == null) {
             String msg = "Bad Request: invalid Pub/Sub message format";
@@ -26,11 +35,16 @@ public class PubSubController {
         }
 
         String data = message.getData();
-        String target = !StringUtils.isEmpty(data) ? new String(Base64.getDecoder().decode(data)) : "World";
-        String msg = "Hello " + target + "!";
+        UserMessage userMessage;
+        try {
+            userMessage = objectMapper.readValue(new String(Base64.getDecoder().decode(data)), UserMessage.class);
+        } catch (JsonProcessingException e) {
+            log.error(e.getMessage());
+            return Mono.just(new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST));
+        }
 
-        log.info(msg);
-        return Mono.just(new ResponseEntity<>(msg, HttpStatus.OK));
+        log.info("UserMessage info: {}", userMessage);
+        return Mono.just(new ResponseEntity<>(userMessage.getBody(), HttpStatus.OK));
     }
 }
 // [END cloudrun_pubsub_handler]
